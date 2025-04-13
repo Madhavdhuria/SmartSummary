a687ff9dc37ade840a048cb13d909a585a9c2f0b
Madhavdhuria <madhavdhuria556@gmail.com>
2025-04-13 23:05:35 +0530

    setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return<Button variant={"outline"} size={"lg"}>ShadCn-button</Button>;
+  return (
+    <>
+      <h1>sommaire</h1>
+      <Button variant={"outline"} size={"lg"}>
+        ShadCn-button
+      </Button>
+      <a href="">hello</a>
+    </>
+  );
 }
