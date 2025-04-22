b02bfefd8d44b05681dca4300f0305916ccfdb32
Madhavdhuria <madhavdhuria556@gmail.com>
2025-04-22 09:18:05 +0530

    SUMMARY_SYSTEM_PROMPT updated

--- app/(logged-in)/summary/[id]/page.tsx
+++ app/(logged-in)/summary/[id]/page.tsx
@@ -1,9 +1,55 @@
+import { getSummary } from "@/lib/summaries";
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 import React from "react";
 
-const Page = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  const summary = await getSummary(id);
 
-  return <div>{id}</div>;
+  if (!summary) notFound();
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-4 flex items-center justify-center">
+      <div className="w-full max-w-3xl">
+        <div className="flex items-center justify-between mb-4">
+          <h1 className="text-2xl lg:text-4xl  lg:tracking-tight font-bold text-red-400">
+            {summary.title}
+          </h1>
+          <div>
+            <p className="text-sm text-gray-500 text-center">
+              Created on{" "}
+              {new Date(summary.created_at).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+            <p className="text-sm text-gray-950 text-center mb-6">
+              {summary.file_name}
+            </p>
+          </div>
+        </div>
+
+        <div className="overflow-x-auto no-scrollbar flex space-x-4 snap-x snap-mandatory border-2 rounded-lg p-3 shadow-md bg-white ">
+          {summary.summary_text}
+        </div>
+
+        <div className="text-center mt-4 p-2 flex justify-around items-center">
+          <button className="bg-rose-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-rose-600 transition duration-200">
+            <Link href="/dashboard">Back to Dashboard</Link>
+          </button>
+          <button className="bg-rose-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-rose-600 transition duration-200">
+            <a href={summary.original_file_url} target="_blank">
+              <ExternalLink className="inline mr-1" />
+              View Original PDF
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- lib/summaries.ts
+++ lib/summaries.ts
@@ -1,7 +1,32 @@
 import { getDbConnection } from "./db";
 
-export async function getSummaries(userId:string) {
-    const sql=await getDbConnection();
-    const summaries=await sql`SELECT * FROM pdf_summary WHERE user_id=${userId} order by created_at desc`;
-    return summaries;
+export async function getSummaries(userId: string) {
+  const sql = await getDbConnection();
+  console.log(Date());
+  const summaries = await sql`
+  SELECT id, title, created_at 
+  FROM pdf_summary 
+  WHERE user_id = ${userId} 
+  ORDER BY created_at DESC
+`;
+
+  console.log(Date());
+
+  return summaries;
+}
+
+export async function getSummary(id: string) {
+  try {
+    const sql = await getDbConnection();
+
+    const [summary] = await sql.query(
+      "SELECT * FROM pdf_summary WHERE id = $1",
+      [id]
+    );
+
+    return summary;
+  } catch (error) {
+    console.error("Error fetching summary:", error);
+    throw new Error("Failed to fetch summary");
+  }
 }
